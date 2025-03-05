c932e2e952f1374b9cf04c5b0e8b1ee0f8ba03ae
User_PC-Oren4 <User_PC-Oren4@PC-OREN4>
2025-03-05 22:53:21 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите строку:"); 
+string input = Console.ReadLine(); 
+string result = WriteLine(input); 
+Console.WriteLine("Итoг: " + result);
+string WriteLine(string input)
+{
+    char[] chars = input.ToCharArray();
+
+    for (int b = 0; b < chars.Length; b++)
+    {
+        if (b % 2 == 0)
+        {
+
+            chars[b] = char.ToUpper(chars[b]);
+        }
+    }
+    return new string(chars);
+}
